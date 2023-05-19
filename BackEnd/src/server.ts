a5668fda90a4e58c11657a25d710c8e32f6fe00a
wesley_wisch <wesley_wisch@hotmail.com>
2023-05-19 10:27:50 -0300

    feat: Creating the route for uploading images and serving these images statically in a folder.

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config'
+import { resolve } from 'node:path'
 import fastify from 'fastify'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import cors from '@fastify/cors'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
 
@@ -12,17 +15,23 @@ app.register(cors, {
   origin: true, // Todos URLs de front-end poderão acessar o back-end
   // origin: ['http://google.com'], // Aqui as urls que estão é as que podem acessar o nosso backend
 })
-
 app.register(jwt, {
   secret: 'spacetime',
 })
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+})
 
-app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 Server running')
