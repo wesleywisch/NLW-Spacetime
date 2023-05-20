2c3b2d3d96201f88470f6761ebb5c555e52019a0
wesley_wisch <wesley_wisch@hotmail.com>
2023-05-20 15:48:21 -0300

    feat: Taking memories and displaying them on screen.

--- FrontEnd/src/components/EmptyMemories.tsx
+++ FrontEnd/src/components/EmptyMemories.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export function EmptyMemories() {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[22.5rem] text-center leading-relaxed">
         Você ainda não registrou nenhuma lembrança, comece a{' '}
         <Link href="/" className="underline hover:text-gray-50">
